79436a3228d17389dde931c8d7125348cd60df94
woxiexienia <s372998620i@gmail.com>
2017-10-18 23:49:47 +0800

    1.01

---
+++ src/components/banana.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{  $route.params.color }}</p>
+    <router-view/>
+    <button @click="getParam">get param</button>
+  </div>  
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: 'i am banana '
+    }
+  },
+  methods: {
+  	getParam (){
+  		console.log(this.$route.params)
+  	}
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,33 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
+import VRouter from 'vue-router'
+import Apple from './components/apple'
+import Banana from './components/banana'
+import RedBanana from './components/redBanana'
+Vue.use(VRouter)
+// Vue.config.productionTip = false
 
-Vue.config.productionTip = false
-
+let router = new VRouter({
+	mode:'history',
+	routes:[
+		{
+			path:'/apple',
+			component: Apple
+		},
+		{
+			path: '/banana',
+			component: Banana,
+			children:[
+				{
+					path:'red',
+					component: Apple
+				}
+			]
+		}
+	]
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
